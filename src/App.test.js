6f9b1413c73b2a68d35e10c0ea3101bf8cdec5cd
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-04-28 02:50:34 +0300

    start to develop tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without crashing test2', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    expect(div.innerHTML).toContain('Hi There!');
+    ReactDOM.unmountComponentAtNode(div)
 });
