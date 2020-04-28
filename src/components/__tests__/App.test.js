773b4763b2a0d2483602387d059eb04eee9bf2bd
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-04-28 22:57:05 +0300

    update

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import App from '../App';
-import CommentBox from "../CommentBox";
+import App from 'components/App';
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 import {shallow} from "enzyme";
 
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<App/>);
+});
+
 it('shows a comment box', function () {
-
-    const wrapper = shallow(<App/>);
     expect(wrapper.find(CommentBox).length).toEqual(1)
+});
 
+it('shows a comment list', function () {
+    expect(wrapper.find(CommentList).length).toEqual(1)
 });
