d966e6aa206f84e5563178c26700beafca38fe64
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-05-16 23:48:12 +0300

    inject provider and store to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
+import {Provider} from 'react-redux'
+import {createStore} from "redux";
+import reducers from './reducers'
 
 ReactDOM.render(
-    <App/>, document.getElementById('root')
+    <Provider store={createStore(reducers)}> <App/></Provider>, document.getElementById('root')
 );
